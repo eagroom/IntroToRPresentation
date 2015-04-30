e6fc923de1968801b25cc4771a04fa392cdb9791
eagroom <eagroom@yahoo.com>
2015-04-30 16:13:10 -0500

    more work

--- Demo script.R
+++ Demo script.R
@@ -1,27 +1,26 @@
 #same as library(plyr) but returns a bool so you cna install it if its not
-if (!require("plyr")) { 
-  install.packages("plyr")
-  require("plyr")
-}
+#if (!require("plyr")) { 
+#  install.packages("plyr")
+#  require("plyr")
+#}
 
-
-setwd("C:\\Users\\Elizabeth\\Documents\\IntroToR\\IntroToRPresentation")
+main <- function(){
+#ssetwd("C:\\Users\\Elizabeth\\Documents\\IntroToR\\IntroToRPresentation")
 surveyResults<-read.csv("Full Results - Stack Overflow Developer Survey - 2015.csv")
 
 #maybe show memory
 
 print(countriesOfOver60(surveyResults))
 
+}
 
-
-countriesOfOver60 <- function(fulldataFrame)
-{
+countriesOfOver60 <- function(fulldataFrame){
   #first shrink the data set down to jsut what we need
   over60data <- subset(fulldataFrame, fulldataFrame$Age == "> 60")
-  #unquieCountries <- ''
+  
   for (i in seq(along = over60data$Country)) {
     country <- as.character(over60data$Country[i])
-    
+    unquieCountries <- c()
     if (!country %in% unquieCountries)
     {
       unquieCountries <- c(unquieCountries, country)
