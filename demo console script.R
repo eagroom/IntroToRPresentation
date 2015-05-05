225fedaddd28fd8f3ef55838ffc36620c56ab890
eagroom <eagroom@yahoo.com>
2015-05-05 15:39:36 -0500

    spell checked and added an NA slide

--- demo console script.R
+++ demo console script.R
@@ -1,7 +1,14 @@
 #Demo
 setwd("C:\\Users\\Elizabeth\\Documents\\IntroToR\\IntroToRPresentation")
+#getwd returns an absolute filepath representing the current working directory of the R process; 
+#  setwd(dir) is used to set the working directory to dir.
+
+#read.table you set the sep ect.
 surveyResults<-read.csv("Full Results - Stack Overflow Developer Survey - 2015.csv")
 str(surveyResults)
+
+#how many repsondants didn't submit an age
+sum(is.na(surveyResults$Age))
 
 #age is a factor so we have to convert so lets take a look at the levels
 levels(surveyResults$Age)
@@ -13,7 +20,7 @@ levels(surveyResults$Age)
 #let's use the dplyr package 
 # -this package has A set of tools that solves a common set of problems
 # packages can be thought of as libraries
-
+?
 #install.packages("plyr") #don't actually do
 library(plyr)
 
